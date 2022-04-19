c62725ce9313920abf5d797bb4b6b8ab62052530
Martin Jorge Nicolas <nicomartin854@gmail.com>
2022-04-18 23:52:04 -0300

    Actualizando README

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extends: true }));
 
 //Puerto de la app
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 //ImPORTar Rutas
 app.use("/api/inscripcion", require("./routes/inscripciones"));
