67d82be8ee9f60fa48524ae27061a6e89d7c4061
Martin Jorge Nicolas <nicomartin854@gmail.com>
2022-04-26 20:16:01 -0300

    Agregando excepciones al controlador inscripcion

--- controllers/inscripcionesController.js
+++ controllers/inscripcionesController.js
@@ -3,12 +3,20 @@ const Inscripciones = require("../models/Inscripciones");
 //Controller para crear un usuario
 exports.altaInscripcion = async (req, res) => {
   const inscripciones = new Inscripciones(req.body);
-  inscripciones.save();
-  res.json({ inscripciones });
+
+  try {
+    await inscripciones.save();
+    res.status(200).json({ inscripciones });
+  } catch (error) {
+    res.status(500).json({ error: "No se pudo completar la inscripcion!" });
+  }
 };
 
 exports.getAllInscripciones = async (req, res) => {
-  const inscripciones = await Inscripciones.find({});
-
-  res.json({ inscripciones });
+  try {
+    const inscripciones = await Inscripciones.find({});
+    res.status(200).json({ inscripciones });
+  } catch (error) {
+    res.status(500).json({ error: "No se pudo acceder a los datos! " });
+  }
 };
